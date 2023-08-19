0f92b809b22e51f5a722362b2af5b913f8556794
aymenachelache <achelache.aymen@gmail.com>
2023-08-20 00:07:11 +0100

    Upload Files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// NavBar Mobile
+
+let bars = document.querySelector(".bars");
+let nav = document.querySelector("ul.nav");
+
+bars.addEventListener("click", (eo) => {
+    nav.classList.toggle("show");
+console.log("red");
+});
