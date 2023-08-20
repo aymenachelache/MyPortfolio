e6d17723680ea7ba56eb83ca3e338d645a2c72cd
aymenachelache <achelache.aymen@gmail.com>
2023-08-20 12:31:44 +0100

    Edit Files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 
 // NavBar Mobile
 
-let bars = document.querySelector(".bars");
+// let bars = document.querySelector(".bars");
 let nav = document.querySelector("ul.nav");
+let header = document.querySelector(".header");
 
-bars.addEventListener("click", (eo) => {
-    nav.classList.toggle("show");
-console.log("red");
-});
+function navBar() {
+    console.log(nav.classList.contains("d-flex"));
+    if (nav.classList.contains("show")) {
+        nav.classList.remove("show");
+    } else {
+        nav.classList.add("show");
+    }
+}
