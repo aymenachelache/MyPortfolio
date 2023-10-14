4cef6519ace6e1f0adf6ccf8d2c3f90561d9b077
aymenachelache <achelache.aymen@gmail.com>
2023-10-14 10:17:38 +0100

    Upload Files

--- js/main.js
+++ js/main.js
@@ -12,3 +12,30 @@ function navBar() {
         nav.classList.add("show");
     }
 }
+
+
+let up = document.querySelector(".up");
+window.onscroll = function () {
+    if (window.scrollY == 0) {
+        up.style.display = "none";
+    } else if (window.scrollY > 100) {
+        up.style.display = "block";
+        up.addEventListener("click", () => {
+            window.scrollTo(0, 0);
+        });
+    }
+}
+
+// Resume List
+let resumeList = document.querySelectorAll('.resume-list li span');
+
+document.querySelectorAll(".resume-list li").forEach( (el) => {
+    el.addEventListener("click", (eo) => {
+        resumeList.forEach((span) => {
+            span.parentElement.classList.remove("active");
+        });
+        el.classList.add("active");
+    });  
+});
+
+console.log(resumeList);
